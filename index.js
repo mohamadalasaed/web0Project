f1c4d3ba87d50b431524cb839da8e4623f962edc
= <=>
2022-12-12 22:55:00 +0200

    added add student inputs

--- index.js
+++ index.js
@@ -4,11 +4,36 @@ fetch('http://localhost:3000/data')
             json.forEach(student => {  
                 var content = `
                     <h3 class="text-center">Name: ${student.name}</h3>
-                    <p>Age: ${student.age}</p>`
+                    <p>Age: ${student.age}</p>`;
 
                 var div = document.createElement('div');
                 div.innerHTML = content;
                 div.className = 'col-sm-6 col-md-3';
                 document.getElementById('r').appendChild(div);
             });
-})
+});
+
+
+function AddStudent(){
+    var namevalue = document.getElementById('txtname').value;
+    var agevalue = document.getElementById('txtage').value;
+    fetch('https://api-service-7fpg.onrender.com/addstudent', 
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'name': namevalue , 'age': agevalue}),
+        }).then(()=>{
+            clearFields();
+            getStudents();
+        })               
+}
+
+function clearFields()
+{
+    document.getElementById('txtname').value ='';
+    document.getElementById('txtage').value ='';
+}
+
+getStudents();
