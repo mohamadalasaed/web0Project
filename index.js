e1af25ee2725762d075f4d1fd6219a5f8dec6cf6
= <=>
2022-12-12 22:58:00 +0200

    fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch('http://localhost:3000/data')
+fetch('https://api-service-7fpg.onrender.com/data')
         .then(response => response.json())
         .then(json => {
             json.forEach(student => {  
